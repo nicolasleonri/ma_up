5c40248476442c6eac190a335af4f36a2182d5ee
nicolasal97 <nicolasal97@curta.zedat.fu-berlin.de>
2026-06-24 00:22:30 +0200

    cleaned

--- 1_corpus_construction.sh
+++ 1_corpus_construction.sh
@@ -31,20 +31,19 @@ mkdir -p logs/slurm
 # module purge
 
 ### 3. VLM
-module purge
-module add virtualenv/20.23.1-GCCcore-12.3.0
-module add Python/3.11.3-GCCcore-12.3.0
-module load CUDA/12.1.1
-module load cuDNN/8.9.2.26-CUDA-12.1.1
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-export HF_HOME=/scratch/nicolasal97/.cache/huggingface
-source venv/corpus_construction/vlm_extraction/bin/activate
-python3 -m src.workflows.vlm_extraction --crops-dir data/corpus_construction/layout_detection/results --gpu-memory-utilization 0.85
+# module purge
+# module add virtualenv/20.23.1-GCCcore-12.3.0
+# module add Python/3.11.3-GCCcore-12.3.0
+# module load CUDA/12.1.1
+# module load cuDNN/8.9.2.26-CUDA-12.1.1
+# export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+# export HF_HOME=/scratch/nicolasal97/.cache/huggingface
+# source venv/corpus_construction/vlm_extraction/bin/activate
+# python3 -m src.workflows.vlm_extraction --crops-dir data/corpus_construction/layout_detection/results --gpu-memory-utilization 0.85
 
-## 4. Evaluate
-module purge
-module add virtualenv/20.32.0-GCCcore-14.3.0
-module add Python/3.13.5-GCCcore-14.3.0
-source venv/corpus_construction/evaluate_extraction/bin/activate
-python3 -m src.workflows.evaluate_extraction --results data/corpus_construction/vlm_extraction/results.parquet --gold data/corpus_construction/evaluate_extraction/gold_standard.csv --output-csv data/corpus_construction/evaluate_extraction/evaluation.csv
-    
+# ## 4. Evaluate
+# module purge
+# module add virtualenv/20.32.0-GCCcore-14.3.0
+# module add Python/3.13.5-GCCcore-14.3.0
+# source venv/corpus_construction/evaluate_extraction/bin/activate
+# python3 -m src.workflows.evaluate_extraction --results data/corpus_construction/vlm_extraction/results.parquet --gold data/corpus_construction/evaluate_extraction/gold_standard.csv --output-csv data/corpus_construction/evaluate_extraction/evaluation.csv    

24f25e69bb23a3e9dabf607f1633f66a5dfb113e
nicolasal97 <nicolasal97@curta.zedat.fu-berlin.de>
2026-07-15 18:50:05 +0200

    running 2021 onwards for most, EC 16 to 19

--- 0_corpus_acquisition.sh
+++ 0_corpus_acquisition.sh
@@ -7,8 +7,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1GB
-#SBATCH --time=40:00:00
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --time=48:00:00
 
 module purge
 module add virtualenv/20.32.0-GCCcore-14.3.0
@@ -19,14 +19,14 @@ source venv/corpus_acquisition/bin/activate
 ##### EL COMERCIO #####
 # python3 -m src.workflows.acquire_corpus \
 #     --newspaper elcomercio \
-#     --start-date 2015-01-01 \
+#     --start-date 2016-04-01 \
 #     --end-date 2019-12-31 # Final date
 
 ##### TODO LO DEMAS #####
-# newspaper = trome, elcomercio, peru21, gestion, ojo
+# newspaper = trome, elcomercio, peru21, gestion, ojo, correo
 python3 -m src.workflows.acquire_corpus \
-    --newspaper trome \
-    --start-date 2022-07-01 \
+    --newspaper correo \
+    --start-date 2020-02-22 \
     --end-date 2026-06-30 # Final date
 
 exit_code=$?

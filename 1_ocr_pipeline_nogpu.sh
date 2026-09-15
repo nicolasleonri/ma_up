dad6a98ffd258750673006bcedc37f80643b1b5b
nicolasal97 <nicolasal97@curta.zedat.fu-berlin.de>
2026-09-15 12:34:22 +0200

    cleaned

--- 1_ocr_pipeline_nogpu.sh
+++ 1_ocr_pipeline_nogpu.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=trome_ocr_extraction_none_nogpu
-#SBATCH --output=logs/slurm/trome_ocr_extraction_none_nogpu_%j.out
+#SBATCH --job-name=enhance_images_data
+#SBATCH --output=logs/slurm/enhance_images_data_%j.out
 #SBATCH --partition=scavenger
 #SBATCH --account=agfritz
 #SBATCH --qos=prio
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
-#SBATCH --mem-per-cpu=6GB
-#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1GB
+#SBATCH --time=02:00:00
 
 # set -euo pipefail
 
@@ -56,13 +56,13 @@ mkdir -p logs/slurm
 # python3 -m src.workflows.evaluate_extraction --results data/corpus_construction/vlm_extraction/results/correo/test.parquet --gold data/corpus_construction/evaluate_extraction/correo.csv --output-csv data/corpus_construction/evaluate_extraction/results/correo/test.csv --enhance-parquet data/corpus_construction/enhance_images/results/correo/enhance_images.parquet --binarize-parquet data/corpus_construction/binarize/correo/none/binarization.parquet 
 
 # ################# DONE ###################
-####### 1. Enhancement #######
-# module purge
-# module add virtualenv/20.32.0-GCCcore-14.3.0
-# module add Python/3.13.5-GCCcore-14.3.0
-# source venv/corpus_construction/enhance_images/bin/activate
+###### 1. Enhancement #######
+module purge
+module add virtualenv/20.32.0-GCCcore-14.3.0
+module add Python/3.13.5-GCCcore-14.3.0
+source venv/corpus_construction/enhance_images/bin/activate
 
-# python3 -m src.workflows.enhance_images --input-dir data/corpus_construction/enhance_images/correo --output-dir data/corpus_construction/enhance_images/results/correo
+python3 -m src.workflows.enhance_images --input-dir data/corpus_construction/enhance_images/correo --output-dir data/corpus_construction/enhance_images/results/correo
 # python3 -m src.workflows.enhance_images --input-dir data/corpus_construction/enhance_images/elcomercio --output-dir data/corpus_construction/enhance_images/results/elcomercio
 # python3 -m src.workflows.enhance_images --input-dir data/corpus_construction/enhance_images/gestion --output-dir data/corpus_construction/enhance_images/results/gestion
 # python3 -m src.workflows.enhance_images --input-dir data/corpus_construction/enhance_images/ojo --output-dir data/corpus_construction/enhance_images/results/ojo
